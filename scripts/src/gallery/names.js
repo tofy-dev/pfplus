e4a9c806b5a6088c6782a475ad42f6ef5700fa9b
tofy <80428886+tofy-dev@users.noreply.github.com>
2022-04-09 09:59:34 -0700

    update image path for gallery

--- scripts/src/gallery/names.js
+++ scripts/src/gallery/names.js
@@ -1,4 +1,4 @@
 export function listImages() {
-    let images = "../../images/gallery/fox.jpg ../../images/gallery/house.jpg ../../images/gallery/mountains.jpg ../../images/gallery/nothern-lights.jpg ../../images/gallery/san-fran.jpg".split(" ");
+    let images = "../images/gallery/fox.jpg ../images/gallery/house.jpg ../images/gallery/mountains.jpg ../images/gallery/nothern-lights.jpg ../images/gallery/san-fran.jpg".split(" ");
     return images;
 }
