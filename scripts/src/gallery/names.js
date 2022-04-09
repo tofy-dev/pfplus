b71b6967d5baf32f43f32b2d5885fe8267debec6
tofy <80428886+tofy-dev@users.noreply.github.com>
2022-04-09 16:00:50 -0700

    fix link (hopefully??)

--- scripts/src/gallery/names.js
+++ scripts/src/gallery/names.js
@@ -1,5 +1,5 @@
 export function listImages() {
-    let link = "https://tofy-dev.github.io/pfplus/images/gallery";
+    let link = "https://tofy-dev.github.io/pfplus/images/gallery/";
     let images = ["fox.jpg", "house.jpg", "mountains.jpg", "northern-lights.jpg", "san-fran.jpg"];
     for (let img in images) {
         images[img] = link+images[img];
